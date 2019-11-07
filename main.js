58739993dbc0eb1b39247f316e13c0eccf8a6d3b
Hector <hmtv16@gmail.com>
2019-11-07 15:44:43 -0600

    Add block scroll

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ $('a, .btn').on('click', function(event) {
     $('html, body').animate({ scrollTop: $(hash).offset().top }, 800);
   }
 });
+
+// Block scroll for mobile device
+$('body').addClass('stop-scrolling');
